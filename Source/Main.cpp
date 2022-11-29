de9ea2b2fba2a408001843619ec49038db3b60ed
cgbsu@github.com <cgeeley@student.bridgew.edu>
2022-11-29 18:38:21 +0000

    Added some nice sliders

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -4,10 +4,10 @@ int main(int argc, char** args)
 {
 	constexpr const auto profile = Profiles::PhysicallyInaccurateSimple;
 	using ScalarType = Constants<profile>::ScalarType;
-	constexpr const ScalarType totalEnergy = .1f;
-	constexpr const ScalarType mass = 1.f;
-	constexpr const auto startRegionParameters = RegionParameters<profile>{1.f, 0.f};
-	constexpr const auto startBoundry = BoundryCoefficientsType<profile> {.4f, .4f};
+	ScalarType totalEnergy = .1f;
+	ScalarType mass = 1.f;
+	const auto startRegionParameters = RegionParameters<profile>{1.f, 0.f};
+	const auto startBoundry = BoundryCoefficientsType<profile> {.4f, .4f};
 	const auto regionParameters = std::array{
 			RegionParameters<profile>{.8f, 3.f}, 
 			RegionParameters<profile>{.5f, 2.f}, 
@@ -58,8 +58,83 @@ int main(int argc, char** args)
 			);
 	}
 	std::cout << waveValues.size() << "\n";
+	float startBondryReal = 0.f;
+	float startBondryImaginary = 0.f;
+	float startBondryPotential = 0.f;
+	float startBondryLength = 0.f;
+	float reasonableMaximum = 1.f;
+
 	auto* window = initializeGUI();
-	renderLoop(window, [&](){
+	renderLoop(window, [&]()
+		{
+			ImGui::VSliderFloat(
+					"Start Boundry (Real Part)", 
+					ImVec2(18, 160), 
+					&startBondryReal, 
+					0.f, 
+					1.0f, 
+					"%.3f", 
+					ImGuiSliderFlags_None
+				);
+			ImGui::SameLine();
+			ImGui::VSliderFloat(
+					"Start Boundry (Imaginary Part)", 
+					ImVec2(18, 160), 
+					&startBondryImaginary, 
+					0.f, 
+					1.0f, 
+					"%.3f", 
+					ImGuiSliderFlags_None
+				);
+			ImGui::VSliderFloat(
+					"Reasonible Maximum", 
+					ImVec2(18, 160), 
+					&reasonableMaximum, 
+					0.f, 
+					100.f, 
+					"%.3f", 
+					ImGuiSliderFlags_None
+				);
+			ImGui::SameLine();
+			ImGui::VSliderFloat(
+					"Total Energy", 
+					ImVec2(18, 160), 
+					&totalEnergy, 
+					0.f, 
+					reasonableMaximum, 
+					"%.3f", 
+					ImGuiSliderFlags_None
+				);
+			ImGui::SameLine();
+			ImGui::VSliderFloat(
+					"Mass", 
+					ImVec2(18, 160), 
+					&mass, 
+					0.f, 
+					reasonableMaximum, 
+					"%.3f", 
+					ImGuiSliderFlags_None
+				);
+			ImGui::SameLine();
+			ImGui::VSliderFloat(
+					"Start Boundry Potential", 
+					ImVec2(18, 160), 
+					&startBondryPotential, 
+					0.f, 
+					totalEnergy, 
+					"%.3f", 
+					ImGuiSliderFlags_None
+				);
+			ImGui::SameLine();
+			ImGui::VSliderFloat(
+					"Start Boundry Length", 
+					ImVec2(18, 160), 
+					&startBondryLength, 
+					0.f, 
+					reasonableMaximum, 
+					"%.3f", 
+					ImGuiSliderFlags_None
+				);
 			plot(waveValues);
 		});
 	cleanUpGUI(window);
