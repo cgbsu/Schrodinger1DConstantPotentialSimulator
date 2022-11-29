7ae3a3debe6da05e55b49924e658c0d6b3512da7
cgbsu@github.com <cgeeley@student.bridgew.edu>
2022-11-29 18:16:41 +0000

    A bit of cleanup.

--- Include/ChartView.hpp
+++ Include/ChartView.hpp
@@ -1,4 +1,9 @@
 #include <Data.hpp>
+#include "imgui.h"
+#include <bindings/imgui_impl_glfw.h>
+#include <bindings/imgui_impl_opengl3.h>
+#include "implot.h"
+#include <GLFW/glfw3.h>
 
 #ifndef SCHRODINGER_1D__CONSTANT__POTENTIAL__SIMULATOR__INCLUDE__GUARD__CHART__VIEW__HPP
 #define SCHRODINGER_1D__CONSTANT__POTENTIAL__SIMULATOR__INCLUDE__GUARD__CHART__VIEW__HPP
@@ -26,18 +31,23 @@ static auto consolidateData(std::vector<Data<ProfileTagParamterConstant>>& regio
 }
 
 template<auto ProfileTagParamterConstant = defaultProfile>
-static void plot(const ChartBuffers<ProfileTagParamterConstant>& buffers)
+static void plot(const std::vector<Data<ProfileTagParamterConstant>>& data)//const ChartBuffers<ProfileTagParamterConstant>& buffers)
 {
 	if(ImGui::Begin("1D Schrodinger Constant Potential Simulation"))
 	{
 		if(ImPlot::BeginPlot("Wave Function"))
 		{
-			ImPlot::PlotScatter(
-					"All Regions", 
-					buffers.xBuffer.data(), 
-					buffers.yBuffer.data(), 
-					buffers.xBuffer.size()
-				);
+			size_t region = 0;
+			for(const auto& buffer : data)
+			{
+				ImPlot::PlotScatter(
+						allToString("Region ", region).c_str(), 
+						buffer.x.data(), 
+						buffer.y.data(), 
+						buffer.x.size()
+					);
+				++region;
+			}
 			ImPlot::EndPlot();
 		}
 		else
@@ -46,6 +56,65 @@ static void plot(const ChartBuffers<ProfileTagParamterConstant>& buffers)
 	else
 		std::cerr << "ImGui failed to begin!\n";
 	ImGui::End();
+}
+
+static void glfw_error_callback(int error, const char* description) {
+    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
+static auto* initializeGUI()
+{
+	glfwSetErrorCallback(glfw_error_callback);
+	if(!glfwInit()) {
+		std::cerr << "GLFW failed to initialize!\n";
+	}
+	auto* window = glfwCreateWindow(1800, 800, "Graph", NULL, NULL);
+	if(!window) {
+		glfwTerminate();
+		std::cout << "GLFW failed to create a window!\n";
+		return window;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 130");
+	ImPlot::CreateContext();
+	//ImGui::GetIO().DisplaySize = ImVec2(400.f, 400.f);
+    ImGui::GetIO().Fonts->Build();
+	return window;
+}
+
+static void cleanUpGUI(auto* window)
+{
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	ImPlot::DestroyContext();
+	ImGui::DestroyContext();
+}
+
+void renderLoop(auto* window, auto job)
+{
+	while(!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();	
+		job();
+        ImGui::Render();
+		int display_w, display_h;
+        //glfwGetFramebufferSize(window, &display_w, &display_h);
+        //glViewport(0, 0, display_w, display_h);
+        glClearColor(255, 255, 255, 255);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		glfwSwapBuffers(window);
+	}
 }
 
 #endif // SCHRODINGER_1D__CONSTANT__POTENTIAL__SIMULATOR__INCLUDE__GUARD__CHART__VIEW__HPP
--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,13 +1,4 @@
 #include <ComputeRegion.hpp>
-#include "imgui.h"
-#include "bindings/imgui_impl_glfw.h"
-#include "bindings/imgui_impl_opengl3.h"
-#include "implot.h"
-#include <GLFW/glfw3.h>
-
-static void glfw_error_callback(int error, const char* description) {
-    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
-}
 
 int main(int argc, char** args)
 {
@@ -67,51 +58,10 @@ int main(int argc, char** args)
 			);
 	}
 	std::cout << waveValues.size() << "\n";
-	const auto buffers = consolidateData<profile>(waveValues);
-	glfwSetErrorCallback(glfw_error_callback);
-	if(!glfwInit()) {
-		std::cerr << "GLFW failed to initialize!\n";
-		return 1;
-	}
-	auto* window = glfwCreateWindow(400, 400, "Graph", NULL, NULL);
-	if(!window)
-	{
-		glfwTerminate();
-		std::cout << "GLFW failed to create a window!\n";
-		return 2;
-	}
-	glfwMakeContextCurrent(window);
-	glfwSwapInterval(1);
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init("#version 130");
-	ImPlot::CreateContext();
-	ImGui::GetIO().DisplaySize = ImVec2(400.f, 400.f);
-    ImGui::GetIO().Fonts->Build();
-	//ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
-	while(!glfwWindowShouldClose(window))
-	{
-		glfwPollEvents();
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();	
-		plot(buffers);
-        ImGui::Render();
-		int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(255, 255, 255, 255);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-		glfwSwapBuffers(window);
-	}
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-	glfwDestroyWindow(window);
-	glfwTerminate();
-	ImPlot::DestroyContext();
-	ImGui::DestroyContext();
+	auto* window = initializeGUI();
+	renderLoop(window, [&](){
+			plot(waveValues);
+		});
+	cleanUpGUI(window);
 }
 
